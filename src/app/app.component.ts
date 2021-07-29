7831c18e070a1d317f1ba5a4a2f3e3dbb6ad97c6
Aliciaegea <aliegea4@gmail.com>
2021-07-29 09:42:22 +0200

    mejora maquetación bootstrap + borrador formulario

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,39 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
+interface Datos {
+  name: string;
+  surname: string;
+  email: string;
+  phone: string;
+  web?: string;
+}
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
+  codes: Datos[] = [];
+
   public values: string;
-  public level: 'L' | 'M' | 'Q' | 'H';
+  public level: "L" | "M" | "Q" | "H";
   public width: number;
   public background: string;
-
+  public data: Datos = {
+    name: "",
+    surname: "",
+    email: "",
+    phone: "",
+    web: "",
+  };
   constructor() {
-    this.level = 'L';
-    this.values = 'QR code string value';
+    this.level = "L";
+    this.values = JSON.stringify(this.data);
     this.width = 200;
-    this.background = 'white';
+    this.background = "white";
   }
 
-  qrLevel(val: 'L' | 'M' | 'Q' | 'H') {
+  qrLevel(val: "L" | "M" | "Q" | "H") {
     this.level = val;
   }
 
@@ -29,16 +44,16 @@ export class AppComponent {
   qrWidth(val: number) {
     this.width = val;
   }
-  qrBackground(val: 'WHITE' | 'BLUE' | 'YELLOW') {
+  qrBackground(val: "WHITE" | "BLUE" | "YELLOW") {
     switch (val) {
-      case 'WHITE':
-        this.background = '#FFFFFF';
+      case "WHITE":
+        this.background = "#FFFFFF";
         break;
-      case 'BLUE':
-        this.background = '#0000FF';
+      case "BLUE":
+        this.background = "#0000FF";
         break;
-      case 'YELLOW':
-        this.background = '#FFFF00';
+      case "YELLOW":
+        this.background = "#FFFF00";
         break;
     }
   }
